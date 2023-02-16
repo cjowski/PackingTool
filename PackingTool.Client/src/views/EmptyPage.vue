d27b997992df7a40dacf5aee88ceae03359e2f11
Marcin Ochocki <marcinos2929@gmail.com>
2023-02-16 22:38:50 +0100

    move client code to new location

---
+++ PackingTool.Client/src/views/EmptyPage.vue
@@ -0,0 +1,18 @@
+<template></template>
+<!-- <template>{{ test }}</template> -->
+
+<script setup lang="ts">
+// import { WeatherForecastService } from '@/api';
+// import { onMounted, ref } from 'vue';
+
+// const test = ref('nothing')
+
+// const fetchWeather = async () => {
+//   const response = await WeatherForecastService.getWeatherForecast()
+//   test.value = JSON.stringify(response)
+// }
+
+// onMounted(() => {
+//   fetchWeather()
+// })
+</script>
